9f10fce8c61764875d8b9a934d02bf39a28c5492
Magnus Germundsson <a16magge@hs.local>
2017-01-24 16:38:11 +0100

    Added item pickup functionality
    Created an item pickup script
    -Destroys the item it picks up
    -Sends the item to a function that in the future will add the item to the
    player's inventory

---
+++ Assets/Scripts/ItemPickupBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ItemPickupBehaviour : MonoBehaviour {
+
+    void TempItemPickupFunction(GameObject item)
+    {
+        // TODO - Add logic here when inventory system exists
+    }
+
+	void OnCollisionEnter2D(Collision2D collision)
+    {
+        // If the player is colliding with an item
+        if (collision.gameObject.tag == "Item")
+        {
+            // Send the item to the inventory
+            TempItemPickupFunction(collision.gameObject);
+
+            // Destory the item instance
+            Destroy(collision.gameObject);
+        }
+    }
+}
