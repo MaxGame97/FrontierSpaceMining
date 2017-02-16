57361bc6705d6440c9b0defeee13f41d686e1d2d
Magnus Germundsson <a16magge@hs.local>
2017-02-16 16:43:55 +0100

    Added a base for an enemy patrol system

--- Assets/Scripts/EnemyBehaviour.cs
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -3,12 +3,20 @@ using System.Collections;
 
 public class EnemyBehaviour : MonoBehaviour {
 
+    private enum Behaviour { idle, recon, patrol, roaming };
+
+    [Header("Enemy movement values")]
+
     [SerializeField] [Range(1f, 15f)] private float acceleration = 2;       // The AI's acceleration speed
     [SerializeField] [Range(1f, 20f)]  private float maxSpeed = 2;          // The AI's max speed
     [SerializeField] [Range(0.01f, 0.1f)] private float rotationSpeed = 2;  // The AI's rotation speed
     [SerializeField] [Range(0.5f, 2.5f)] private float minDistance = 1f;    // The AI's minimum distance to the player
     [SerializeField] [Range(1f, 5)] private float bulletAliveTime = 5f;     // The Ai's speed of their bullets 
     [SerializeField] [Range(1f, 10)] private float EMPduration = 5f;        // The Ai's speed of their bullets 
+
+    [Space(2f)]
+
+    [Header("Enemy stealth values")]
 
     [SerializeField] [Range(5f, 25f)] private float hearingValue = 10f;     // The AI's hearing value, the lower it is the faster the AI will detect the player 
     [SerializeField] [Range(0f, 5f)] private float enemyIntelligence = 0;   // The AI's intelligence, the higher this value is the faster the AI will detect the player
@@ -17,6 +25,20 @@ public class EnemyBehaviour : MonoBehaviour {
     [SerializeField] [Range(5f, 25f)] private float viewDistance;           // The enemy's view distance
     [SerializeField] GameObject viewCone;                                   // The prefab object used for the view cone
     [SerializeField] GameObject bullet;                                     // The prefab object used for the bullet
+
+    [Space(2f)]
+    
+    [Header("Enemy patrol/roaming values")]
+
+    [SerializeField] private Behaviour startingBehaviour;
+
+    [SerializeField] private Transform[] patrolNodes;
+
+    [SerializeField] private float roamingRadius;
+
+    [Space(2f)]
+
+    [Header("Enemy misc values")]
 
     [SerializeField] GameObject soundFXPrefab;                              // The sound FX prefab
 
@@ -40,6 +62,7 @@ public class EnemyBehaviour : MonoBehaviour {
 
     private State currentState;                                             // The enemy's current state
     private IdleState idleState;                                            // An idlestate instance
+    private PatrolState patrolState;
     private AlertState alertState;                                          // An alertstate instance
     private SearchingState searchingState;                                  // A searchingstate instance
     private DisabledState disabledState;                                    // A disabledstate instance
@@ -83,6 +106,63 @@ public class EnemyBehaviour : MonoBehaviour {
         }
     }
 
+    private class PatrolState : State
+    {
+        // The current enemybehaviour instance
+        EnemyBehaviour enemy;
+
+        int patrolIndex;
+
+        public PatrolState(EnemyBehaviour enemy)
+        {
+            this.enemy = enemy;
+
+            patrolIndex = 0;
+        }
+
+        public override void Entry()
+        {
+            // Change the view cone color to blue
+            enemy.viewConeSprite.color = new Color(0f / 255f, 120f / 255f, 255f / 255f);
+            // Increase the drag so that the enemy will stop
+            enemy.enemyRigidbody.drag = 0.75f; // TODO - Make this solution better
+        }
+
+        public override void Update()
+        {
+            enemy.range = Vector3.Distance(enemy.transform.position, enemy.patrolNodes[patrolIndex].position);
+
+            // If the enemy is too far away from the player's last position
+            if (enemy.range > enemy.minDistance)
+            {
+                enemy.MoveTowards(enemy.patrolNodes[patrolIndex]);
+            }
+            else
+            {
+                if (patrolIndex == enemy.patrolNodes.Length)
+                    patrolIndex = 0;
+                else
+                    patrolIndex++;
+            }
+
+            // If the enemy can hear the player, enter alert state
+            //if (enemy.CanHearPlayer())
+                //Exit(enemy.searchingState);
+            // If the enemy can see the player, enter the alert state
+            //if (enemy.CanSeePlayer())
+                //Exit(enemy.alertState);
+        }
+
+        public override void Exit(State exitState)
+        {
+            // Remove the drag so that the enemy feels more "spacey"
+            enemy.enemyRigidbody.drag = 0f; // TODO - Make this solution better
+
+            enemy.currentState = exitState;
+            exitState.Entry();
+        }
+    }
+
     // When the enemy is in the alert state, it is chasing the player until it loses sight of them
     private class AlertState : State
     {
@@ -109,27 +189,10 @@ public class EnemyBehaviour : MonoBehaviour {
 
         public override void Update()
         {
-            // TODO - Maybe add a more sophisticated movement system
-
-            // Get the distance to the player
-            enemy.range = Vector2.Distance(enemy.transform.position, enemy.target.position);
-
-            // If the enemy is too far away from the player
+            // If the enemy is too far away from the player's last position
             if (enemy.range > enemy.minDistance)
             {
-                //Move AI towards player
-                enemy.enemyRigidbody.AddForce(enemy.transform.up * enemy.acceleration);
-
-                // Clamp the player's velocity to the max speed
-                enemy.enemyRigidbody.velocity = Vector2.ClampMagnitude(enemy.enemyRigidbody.velocity, enemy.maxSpeed);
-
-                //Find what to rotate towards and reset x and y value since we dont want to ratate those axis
-                enemy.newRotation = Quaternion.LookRotation(enemy.transform.position - enemy.target.position, Vector3.forward);
-                enemy.newRotation.x = 0.0f;
-                enemy.newRotation.y = 0.0f;
-
-                //rotate AI towards player
-                enemy.transform.rotation = Quaternion.Slerp(enemy.transform.rotation, enemy.newRotation, enemy.rotationSpeed);
+                enemy.MoveTowards(enemy.target);
             }
 
             if (!enemy.IsInvoking("Shoot"))
@@ -187,19 +250,7 @@ public class EnemyBehaviour : MonoBehaviour {
             // If the enemy is too far away from the player's last position
             if (enemy.range > enemy.minDistance)
             {
-                //Move AI towards player
-                enemy.enemyRigidbody.AddForce(enemy.transform.up * enemy.acceleration);
-
-                // Clamp the player's velocity to the max speed
-                enemy.enemyRigidbody.velocity = Vector2.ClampMagnitude(enemy.enemyRigidbody.velocity, enemy.maxSpeed / 2);
-
-                //Find what to rotate towards and reset x and y value since we dont want to ratate those axis
-                enemy.newRotation = Quaternion.LookRotation(enemy.transform.position - lastSeenPosition, Vector3.forward);
-                enemy.newRotation.x = 0.0f;
-                enemy.newRotation.y = 0.0f;
-
-                //rotate AI towards player
-                enemy.transform.rotation = Quaternion.Slerp(enemy.transform.rotation, enemy.newRotation, enemy.rotationSpeed);
+                enemy.MoveTowards(enemy.target);
             }
             // If the enemy has reached the player's last position, exit to the idle state
             else
@@ -267,6 +318,30 @@ public class EnemyBehaviour : MonoBehaviour {
         }
     }
 
+    void MoveTowards(Transform swag)
+    {
+        // Get the distance to the player
+        range = Vector2.Distance(transform.position, swag.position);
+
+        // If the enemy is too far away from the player
+        if (range > minDistance)
+        {
+            //Move AI towards player
+            enemyRigidbody.AddForce(transform.up * acceleration);
+
+            // Clamp the player's velocity to the max speed
+            enemyRigidbody.velocity = Vector2.ClampMagnitude(enemyRigidbody.velocity, maxSpeed);
+
+            //Find what to rotate towards and reset x and y value since we dont want to ratate those axis
+            newRotation = Quaternion.LookRotation(transform.position - swag.position, Vector3.forward);
+            newRotation.x = 0.0f;
+            newRotation.y = 0.0f;
+
+            //rotate AI towards player
+            transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotationSpeed);
+        }
+    }
+
     void Shoot()
     {
         GameObject tempBullet = (GameObject)Instantiate(bullet, transform.position, transform.rotation);
@@ -284,6 +359,7 @@ public class EnemyBehaviour : MonoBehaviour {
     void Awake()
     {
         idleState = new IdleState(this);            // Instantiates the idle state
+        patrolState = new PatrolState(this);
         alertState = new AlertState(this);          // Instantiates the alert state
         searchingState = new SearchingState(this);  // Instantiates the searching state
         disabledState = new DisabledState(this);    // Instantiates the disabled state
@@ -310,7 +386,10 @@ public class EnemyBehaviour : MonoBehaviour {
         viewConeTransform.Translate(0f, viewConeSprite.bounds.extents.y, 5f);   // Translate the view cone to border the enemy's front side
         viewConeTransform.parent = transform;                                   // Parent the enemy to the view cone, this will make the view cone follow the enemy
 
-        currentState = idleState;                                               // Set the idle state as the current state               
+        if (startingBehaviour == Behaviour.idle)
+            currentState = idleState;                                           // Set the idle state as the current state    
+        else if (startingBehaviour == Behaviour.patrol)
+            currentState = patrolState;           
         currentState.Entry();                                                   // Run the current state's entry function
     }
 
