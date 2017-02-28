456e47722b8333a114f9efa41d0a5a5636f073e2
Rasmus Lindberg <a16rasli@hs.local>
2017-02-28 14:47:00 +0100

    Added Cannon script and gameobject in Test-scene
    Cannon animations and stuffs. Cannon kills you

---
+++ Assets/Scripts/CannonBehaviour.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CannonBehaviour : MonoBehaviour {
+
+    private Animator anim;
+
+    private bool shooting = false;
+
+    void Start()
+    {
+        anim = GetComponentInParent<Animator>();
+    }
+
+    void Update()
+    {
+        if (anim.GetCurrentAnimatorStateInfo(0).IsName("CannonShoot"))
+        {
+            shooting = true;
+        }
+        else
+        {
+            shooting = false;
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player" && shooting)
+        other.gameObject.GetComponent<PlayerBehaviour>().KillPlayer();
+    }
+
+}
