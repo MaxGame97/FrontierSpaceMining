f84a57dd9bdf18605004d189cc53dff12d9c2d02
Kristian Jonsson <a16krijo@hs.local>
2017-03-07 16:35:37 +0100

    Level 100 Implemented
    Added the scene level_100
    Added rotating lasers, with basic sprites and two related scripts.
    Added doors, with 3 sprites for their different states.
    Added a fusion laser which pulsates, and kills the player on impact.
    Added a scenic script for spawning ships from the hangar.
    Placed objective points which interact with the level, and lets the player
    win in the end.
    Added textures, sprites, and prefabs for the level.

---
+++ Assets/Scripts/Level Design/Level 100/LaserRotor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserRotor : MonoBehaviour
+{
+   
+    [SerializeField] private float rotationSpeed;
+
+
+    private void Update()
+    {
+        transform.Rotate(new Vector3(0, 0, rotationSpeed * Time.deltaTime));
+    }
+
+}
