1a8382d3c00cd0df8efda61305efac606e7e888f
AgilityLars <a16olika@hs.local>
2017-03-08 11:15:41 +0100

    Added Laseranimatoin prefab
    This laser is used for the final level but can be placed anywhere.
    
    The intervall stands for time inbetween each laser "shot" and the
    duration stand for how long each "shot" lasts.

---
+++ Assets/Scripts/Misc/FinalLevelLaser.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FinalLevelLaser : MonoBehaviour {
+    
+    [SerializeField] [Range(0f, 25f)] private float duration = 2;   // Duration of the laser
+    [SerializeField] [Range(1f, 50f)] private float intervall = 10; // Time between each laser shot
+    
+    private MeshRenderer BigLaser;                                  // The MeshRenderer of the mining laser
+
+    [SerializeField] private bool isEnabled = true;
+
+    private float timer;
+
+    void Start()
+    {
+        BigLaser = gameObject.GetComponent<MeshRenderer>();      // Get the MeshRenderer component
+        BigLaser.enabled = false;                                // Disable the MeshRenderer
+    }
+    void FixedUpdate()
+    {
+        timer += Time.deltaTime;
+
+        // If the timer runs out, the laser coroutine is started
+        if (timer > intervall)
+        {
+            StopCoroutine("FireLaser");
+            StartCoroutine("FireLaser");
+        }
+        if(timer > intervall + duration)
+        {
+            StopCoroutine("FireLaser");
+            timer = 0;
+        }
+    }
+
+    IEnumerator FireLaser()
+    {
+        // Enables line so the lineRenderer is active while the timer has not run out
+        BigLaser.enabled = true;
+
+        // As long as the timer has not run out
+        while (timer < intervall + duration && isEnabled)
+        {
+            //Do nothing while laser is shooting
+            yield return null;
+        }
+
+        // Removes line if timer runs out
+        BigLaser.enabled = false;
+    }
+}
---
+++ Assets/Scripts/Misc/MeshRendererAnimation.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MeshRendererAnimation : MonoBehaviour {
+
+	
+    [SerializeField] private int animationRows = 1;     // The amount of rows in the animation
+    [SerializeField] private int animationColumns = 1;  // The amount of columns in the animation
+
+    [SerializeField] [Range(0.1f, 2f)] private float maxInterval = 0.5f; // The max interval (in seconds)
+
+    private int maxIndex;           // The max cell index
+    private int currentIndex;       // The current cell index
+    
+    private float currentInterval;  // The current inverval (in seconds)
+
+    private Vector2 scale;
+
+    void Start()
+    {
+        // Set the max and current index
+        maxIndex = animationRows * animationColumns;
+        currentIndex = 0;
+
+        // Set the current interval to the max interval
+        currentInterval = maxInterval;
+
+        // Calculate the scale of the animation cells
+        scale = new Vector2(1f / animationRows, 1f / animationColumns);
+    }
+
+    void Update() {
+        // Decrease the interval
+        currentInterval -= Time.timeScale;
+
+        // If the interval has expired
+        if(currentInterval < 0)
+        {
+            // Increase the current animation index
+            currentIndex++;
+
+            // If the animation index has reached the max index, reset it
+            if (currentIndex == maxIndex)
+                currentIndex = 0;
+
+            // Reset the current interval
+            currentInterval = maxInterval;
+        }
+
+        // Get the cell index based on the current index
+        Vector2 cellIndex = new Vector2(currentIndex % animationRows, currentIndex % animationColumns);
+
+        // Calculate the offset based on the index and scale, invert the y axis
+        Vector2 offset = new Vector2(cellIndex.x * scale.x, 1f - scale.y - cellIndex.y * scale.y);
+
+        // Set the line renderer's offset and scale based on the calculated values
+        GetComponent<MeshRenderer>().material.SetTextureOffset("_MainTex", offset);
+        GetComponent<MeshRenderer>().material.SetTextureScale("_MainTex", scale);
+    }
+}
