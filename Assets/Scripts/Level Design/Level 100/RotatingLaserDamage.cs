dcfd4f27340e93e0a48fb9c053364ff6d2519dfe
Kristian Jonsson <a16krijo@hs.local>
2017-03-08 17:35:06 +0100

    Polished the testable levels
    Made changes to corridors in level 100
    Added hidden tunnels.
    
    Modified level 002 to make the objective mineable.

--- Assets/Scripts/Level Design/Level 100/RotatingLaserDamage.cs
+++ Assets/Scripts/Level Design/Level 100/RotatingLaserDamage.cs
@@ -4,14 +4,22 @@ using UnityEngine;
 
 public class RotatingLaserDamage : MonoBehaviour
 {
-    public float damage;
-
+    [SerializeField] private float entryDamage = 50;            // The damage the player takes if it collides with the laser beam
+    [SerializeField] private float damagePerSecond = 100;       // The damage per second the player takes while staying in the laser beam
 
     private void OnTriggerEnter2D(Collider2D collider)
     {
         if (collider.gameObject.tag == "Player")
         {
-            collider.gameObject.GetComponent<PlayerBehaviour>().DealDamage(damage);
+            collider.gameObject.GetComponent<PlayerBehaviour>().DealDamage(entryDamage);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            collider.gameObject.GetComponent<PlayerBehaviour>().DealDamage(damagePerSecond * Time.deltaTime);
         }
     }
 }
