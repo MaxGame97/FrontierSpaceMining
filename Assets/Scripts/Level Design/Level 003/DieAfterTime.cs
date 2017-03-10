24da62f4335d49ce49eeb4831996e439270987f5
HenkaBoy <henrik.syvertsson@gmail.com>
2017-03-10 17:01:51 +0100

    Made level 3
    added mining drones, laser fence with gates, conveyor belt..
    and added keyboard buttons for tutorial.

---
+++ Assets/Scripts/Level Design/Level 003/DieAfterTime.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DieAfterTime : MonoBehaviour {
+
+    [SerializeField]
+    float deathTimer;
+    private float count;
+
+    private void Update()
+    {
+        count += Time.deltaTime;
+        if (count >= deathTimer)
+        {
+            transform.parent.parent.parent.GetComponentInChildren<ConveyorBeltBehaviour>().movingObjectsList.Remove(gameObject.transform);
+            Destroy(gameObject);
+        }
+    }
+}
