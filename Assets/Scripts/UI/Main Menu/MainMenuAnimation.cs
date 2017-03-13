2cf6692f2a07da73a8c949f12b87c7d3c7ba5cb5
Rasmus Lindberg <a16rasli@hs.local>
2017-03-13 11:20:22 +0100

    Main Menu Updated
    Updated:
     - New sprites on the different menus.
     - Lowered the amount of menus to only 2 (3 including main menu)
     - Load/new game merged into "Play" menu. You can both delete and start new games on the same screen
     - Options is now only Audio options. No more options needed
     - Animations changed to sync with the different menus.
     - Also updated animations

---
+++ Assets/Scripts/UI/Main Menu/MainMenuAnimation.cs
@@ -0,0 +1,78 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuAnimation : MonoBehaviour {
+
+    private Animator mainPanel;
+    private Animator playPanel;
+    private Animator optionsPanel;
+
+    private bool mainShowing = true;
+    private bool mainSide = false;
+    private bool playShowing = false;
+    private bool optionsShowing = false;
+
+
+    void Start()
+    {
+        mainPanel = GameObject.Find("MainMenuPanel").GetComponent<Animator>();
+        playPanel = GameObject.Find("PlayGamePanel").GetComponent<Animator>();
+        optionsPanel = GameObject.Find("OptionsPanel").GetComponent<Animator>();
+    }
+
+    public void TogglePlayPanel()
+    {
+        if (playShowing)
+        {
+            playPanel.SetTrigger("Close");
+            mainPanel.SetTrigger("MoveToRight");
+            playShowing = false;
+            mainSide = false;
+        }
+        else
+        {
+            if (optionsPanel && mainSide)
+            {
+                optionsPanel.SetTrigger("Close");
+                playPanel.SetTrigger("Open");
+                optionsShowing = false;
+            }
+            else
+            {
+                mainPanel.SetTrigger("MoveToLeft");
+                playPanel.SetTrigger("InstantOpen");
+                mainSide = true;
+            }
+            playShowing = true;
+        }
+    }
+
+    public void ToggleOptionsPanel()
+    {
+        if (optionsShowing)
+        {
+            optionsPanel.SetTrigger("Close");
+            mainPanel.SetTrigger("MoveToRight");
+            optionsShowing = false;
+            mainSide = false;
+        }
+        else
+        {
+            if (playPanel && mainSide)
+            {
+                playPanel.SetTrigger("Close");
+                optionsPanel.SetTrigger("Open");
+                playShowing = false;
+            }
+            else
+            {
+                mainPanel.SetTrigger("MoveToLeft");
+                optionsPanel.SetTrigger("InstantOpen");
+                mainSide = true;
+            }
+            optionsShowing = true;
+        }
+    }
+
+}
