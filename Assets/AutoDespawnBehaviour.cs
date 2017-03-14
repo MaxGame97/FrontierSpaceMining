60a44e2482ff9207265dc2d74ff132ad59c1145a
Kristian Jonsson <a16krijo@hs.local>
2017-03-14 18:55:07 +0100

    Started working on wall visuals
    Added sprites for walls.
    Added fusion core sprite and prefab.
    Added terminals.
    Finished tweaking walls.

---
+++ Assets/AutoDespawnBehaviour.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AutoDespawnBehaviour : MonoBehaviour {
+
+    [SerializeField] private float despawnTimer;        // The time in seconds until the gameObject despawns
+
+
+	void Update ()
+    {
+        // Reduce the value of despawnTimer by time.deltaTime
+        despawnTimer -= Time.deltaTime;
+        // If the value of despawnTimer is zero or less
+        if (despawnTimer <= 0)
+        {
+            // Destroy this gameObject
+            Destroy(gameObject);
+        }
+	}
+}
